88dc3cac5f02f28571ddd4a655234230d7ef9f34
gabrielerzinger <erzingergabriel@outlook.com>
2018-01-29 17:10:22 +0000

    Update readme

--- YelpRep/versionTwo/app.js
+++ YelpRep/versionTwo/app.js
@@ -78,7 +78,7 @@ app.get("/reps/:id", function(rq, rs){
     });
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port , function(){
+
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Yelp Camp Server has started.");
 });
