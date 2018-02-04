1a71144f6120738a63127503813c7c8ca25aad9c
gabrielerzinger <erzingergabriel@outlook.com>
2018-02-04 03:07:45 +0000

    deployed yelp

---
+++ YelpRep/DeployedVersion/app.js
@@ -0,0 +1,59 @@
+//Requires setup
+var bodyParser     = require("body-parser"),
+    Comment        = require("./models/comment"),
+    commentRoutes  = require("./routes/comments"),
+    express        = require("express"),
+    flash          = require("connect-flash"),
+    indexRoutes    = require("./routes/index"),
+    LocalStrategy  = require("passport-local"),
+    LocalMongoose  = require("passport-local-mongoose"),
+    mongoose       = require("mongoose"),
+    methodOverride = require("method-override"),
+    passport       = require("passport"),
+    Rep            = require("./models/rep"),
+    repsRoutes     = require("./routes/reps"),
+    seedDB         = require("./seeds"),
+    User           = require("./models/user");
+
+//App+express setup
+var app = express();
+app.use(flash());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+//Authentication setup
+app.use(require("express-session")({
+    secret            : "Violent delights have violent endings",
+    resave            : false,
+    saveUninitialized : false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Connect and seed the database
+mongoose.connect("mongodb://localhost/yelp_rep");
+//seedDB(); 
+
+//Pass currUser to all templates
+app.use(function(req, res, next){
+    res.locals.currUser = req.user;
+    res.locals.error    = req.flash("error");
+    res.locals.success  = req.flash("success");
+    next();
+});
+
+//Require routes
+app.use(indexRoutes);
+app.use("/reps/:id/comments",commentRoutes);
+app.use("/reps", repsRoutes);
+
+//c9.io configurations
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Yelp Camp Server has started.");
+});
+
---
+++ YelpRep/DeployedVersion/routes/comments.js
@@ -0,0 +1,86 @@
+var express = require("express");
+var router  = express.Router({mergeParams:true});
+var Rep     = require("../models/rep");
+var Comment = require("../models/comment");
+var middleware = require("../middleware");
+
+//Comments new form
+router.get("/new", middleware.isLoggedIn ,function(rq, rs) 
+{
+    Rep.findById(rq.params.id, function(err, rep){
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            rs.render("comments/new", {rep:rep});
+        }
+    });
+});
+
+//Comments create
+router.post("/", middleware.isLoggedIn, function(rq, rs){
+   Rep.findById(rq.params.id, function(err, rep){
+       if(err || !rep) {
+           rq.flash("error", "Something went wrong :-(");
+           rs.redirect("/reps");
+       }
+       else{
+           Comment.create(rq.body.comment, function(err, comment){
+               if(err){
+                   console.log(err);
+                   rs.redirect("/reps");
+               }
+               else{
+                   //add username and id to comment
+                   comment.author.id = rq.user._id;
+                   comment.author.username = rq.user.username;
+                   comment.save();
+                   rep.comments.push(comment._id);
+                   rep.save();
+                   rq.flash("success", "Successfully added comment");
+                   rs.redirect("/reps/" + rep._id);
+               }
+           })
+       }
+   }) 
+});
+
+router.get("/:comments_id/edit", middleware.checkComOwnership,function(rq,rs){
+    Comment.findById(rq.params.comments_id, function(err, foundCom){
+       if(err){
+           rs.redirect("back");
+       } 
+       else{
+            console.log(foundCom);
+            rs.render("comments/edit.ejs", {repid: rq.params.id, comment: foundCom});
+       }
+    });
+});
+
+router.put("/:comments_id", middleware.checkComOwnership,function(rq,rs){
+   Comment.findByIdAndUpdate(rq.params.comments_id, rq.body.comment, function(err, newCom){
+       if(err){
+           rs.redirect("back");
+       }
+       else{
+           rs.redirect("/reps/" + rq.params.id);
+       }
+   });
+});
+
+router.delete("/:comments_id", middleware.checkComOwnership,function(rq,rs){
+   Comment.findByIdAndRemove(rq.params.comments_id, function(err){
+       if(err){
+           rs.redirect("back");
+       }
+       else{
+           rq.flash("success", "Comment deleted.");
+           rs.redirect("/reps/"+rq.params.id);
+       }
+   });
+    
+});
+
+
+module.exports = router;
